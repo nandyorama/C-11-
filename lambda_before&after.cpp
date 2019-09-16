dfc1d22bb3d170ef3a659577cd0e73d30bc72975
pramod <pramod.nandy06@gmail.com>
2019-09-16 23:16:22 +0530

    Update lambda_before&after.cpp

--- lambda_before&after.cpp
+++ lambda_before&after.cpp
@@ -9,20 +9,14 @@ struct printfunc
     }
 };
 int main()
-{
-    auto f = [](){
-        fun();
-        fun1();
-    };
-    f();
-    
-    //2 using functor i.e before c++11
+{    
+    //1 using functor i.e before c++11
     std::vector<int> vi;
     vi.push_back(1);
     vi.push_back(2);
     for_each(vi.begin(),vi.end(),printfunc());
 
-    //3 in C++11
+    //2 in C++11
     struct 
     {
         void operator()(int x)const{
@@ -34,5 +28,12 @@ int main()
     for_each(vi.begin(),vi.end(),[](int x){
         cout<<x<<endl;
     });
+    
+    //3 Nested
+    auto f = [](){
+        fun();
+        fun1();
+    };
+    f();
     return 0;
 }
