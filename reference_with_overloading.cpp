24840ea6678711915d83b7277be760654e118c1e
pramod <pramod.nandy06@gmail.com>
2020-10-17 15:58:11 +0200

    Update reference_with_overloading.cpp

--- reference_with_overloading.cpp
+++ reference_with_overloading.cpp
@@ -24,9 +24,16 @@ int main()
 {
     int j = 20;
     pReference(j);
+
+    int&& x = 1;
+    pReference(x);            
+
     const int i = 10;
+    pReference(i); //Only by pReference(const int & value)
     //Below functions would call pReference(const int & value) if pReference(int && value) wasn't provided
-    pReference(i); 
+    pReference(std::move(j));
+    pReference(std::move(x));
+    pReference(std::move(i));
     pReference(getValue());
     pReference(3);
     return 0;
